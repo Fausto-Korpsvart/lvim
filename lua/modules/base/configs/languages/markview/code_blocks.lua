d6b4c6e3d73a5cdb03f4cd8010a7f5d4b01bc235
LVIM ORG <lvim@motion-agency.com>
2024-12-08 21:29:16 +0200

    Improvements

--- lua/modules/base/configs/languages/markview/code_blocks.lua
+++ lua/modules/base/configs/languages/markview/code_blocks.lua
@@ -1,4 +1,13 @@
 return {
     enable = true,
-    icons = "devicons"
+    icons = "devicons",
+    style = "language",
+    language_direction = "left",
+    min_width = 60,
+    pad_char = " ",
+    pad_amount = 3,
+    hl = "MarkviewCode",
+    info_hl = "MarkviewCodeInfo",
+    sign = true,
+    sign_hl = nil,
 }
