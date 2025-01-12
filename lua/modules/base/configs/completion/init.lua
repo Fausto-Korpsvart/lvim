24bbd5b1fc6d499693e9c0d7cb1565a828a7de12
LVIM ORG <lvim@motion-agency.com>
2025-01-12 17:57:56 +0200

    Blink config

--- lua/modules/base/configs/completion/init.lua
+++ lua/modules/base/configs/completion/init.lua
@@ -7,10 +7,13 @@ config.blink_cmp = function()
     end
     local icons = require("configs.base.ui.icons")
     local lsp_symbols = icons.cmp
-    local ext = { "lazydev", "ripgrep" }
+    -- local ext = { "lazydev", "ripgrep", "emoji", "dictionary" }
+    local ext = { "lazydev", "ripgrep", "emoji" }
     local default_sources = vim.list_extend({ "lsp", "path", "snippets", "buffer" }, ext)
+    local trigger_text = ";"
     blink_cmp.setup({
         snippets = {
+            preset = "luasnip",
             expand = function(snippet)
                 require("luasnip").lsp_expand(snippet)
             end,
@@ -38,7 +41,66 @@ config.blink_cmp = function()
             end,
             providers = {
                 lsp = {
-                    fallbacks = { "lazydev" },
+                    name = "lsp",
+                    enabled = true,
+                    module = "blink.cmp.sources.lsp",
+                    fallbacks = { "lazydev", "snippets", "buffer" },
+                    score_offset = 90,
+                },
+                path = {
+                    name = "Path",
+                    module = "blink.cmp.sources.path",
+                    score_offset = 25,
+                    fallbacks = { "snippets", "buffer" },
+                    opts = {
+                        trailing_slash = false,
+                        label_trailing_slash = true,
+                        get_cwd = function(context)
+                            return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+                        end,
+                        show_hidden_files_by_default = true,
+                    },
+                },
+                buffer = {
+                    name = "Buffer",
+                    enabled = true,
+                    max_items = 3,
+                    module = "blink.cmp.sources.buffer",
+                    min_keyword_length = 4,
+                    score_offset = 15,
+                },
+                snippets = {
+                    name = "snippets",
+                    enabled = true,
+                    max_items = 8,
+                    min_keyword_length = 2,
+                    module = "blink.cmp.sources.snippets",
+                    score_offset = 85,
+                    should_show_items = function()
+                        local col = vim.api.nvim_win_get_cursor(0)[2]
+                        local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+                        return before_cursor:match(trigger_text .. "%w*$") ~= nil
+                    end,
+                    transform_items = function(_, items)
+                        local col = vim.api.nvim_win_get_cursor(0)[2]
+                        local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
+                        local trigger_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
+                        if trigger_pos then
+                            for _, item in ipairs(items) do
+                                item.textEdit = {
+                                    newText = item.insertText or item.label,
+                                    range = {
+                                        start = { line = vim.fn.line(".") - 1, character = trigger_pos - 1 },
+                                        ["end"] = { line = vim.fn.line(".") - 1, character = col },
+                                    },
+                                }
+                            end
+                        end
+                        vim.schedule(function()
+                            require("blink.cmp").reload("snippets")
+                        end)
+                        return items
+                    end,
                 },
                 lazydev = {
                     name = "LazyDev",
@@ -66,6 +128,41 @@ config.blink_cmp = function()
                         end,
                     },
                 },
+                emoji = {
+                    module = "blink-emoji",
+                    name = "Emoji",
+                    score_offset = 15,
+                    opts = { insert = true },
+                },
+                -- dictionary = {
+                --     module = "blink-cmp-dictionary",
+                --     name = "Dict",
+                --     score_offset = 20,
+                --     enabled = true,
+                --     max_items = 8,
+                --     min_keyword_length = 3,
+                --     opts = {
+                --         get_command = {
+                --             "rg",
+                --             "--color=never",
+                --             "--no-line-number",
+                --             "--no-messages",
+                --             "--no-filename",
+                --             "--ignore-case",
+                --             "--",
+                --             "${prefix}",
+                --             vim.fn.expand("~/github/dotfiles-latest/dictionaries/words"),
+                --         },
+                --         documentation = {
+                --             enable = true,
+                --             get_command = {
+                --                 "wn",
+                --                 "${word}",
+                --                 "-over",
+                --             },
+                --         },
+                --     },
+                -- },
             },
         },
         appearance = {
@@ -88,32 +185,8 @@ config.blink_cmp = function()
                     },
                     components = {
                         label = {
-                            width = { fill = true, max = 60 },
-                            text = function(ctx)
-                                local highlights_info = require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
-                                if highlights_info ~= nil then
-                                    return highlights_info.text
-                                else
-                                    return ctx.label
-                                end
-                            end,
-                            highlight = function(ctx)
-                                local highlights_info = require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
-                                local highlights = {}
-                                if highlights_info ~= nil then
-                                    for _, info in ipairs(highlights_info.highlights) do
-                                        table.insert(highlights, {
-                                            info.range[1],
-                                            info.range[2],
-                                            group = ctx.deprecated and "BlinkCmpLabelDeprecated" or info[1],
-                                        })
-                                    end
-                                end
-                                for _, idx in ipairs(ctx.label_matched_indices) do
-                                    table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
-                                end
-                                return highlights
-                            end,
+                            text = require("colorful-menu").blink_components_text,
+                            highlight = require("colorful-menu").blink_components_highlight,
                         },
                     },
                 },
@@ -165,6 +238,172 @@ config.blink_cmp = function()
         },
     })
 end
+
+-- config.blink_cmp = function()
+--     local blink_cmp_status_ok, blink_cmp = pcall(require, "blink.cmp")
+--     if not blink_cmp_status_ok then
+--         return
+--     end
+--     local icons = require("configs.base.ui.icons")
+--     local lsp_symbols = icons.cmp
+--     local ext = { "lazydev", "ripgrep" }
+--     local default_sources = vim.list_extend({ "lsp", "path", "snippets", "buffer" }, ext)
+--     blink_cmp.setup({
+--         snippets = {
+--             expand = function(snippet)
+--                 require("luasnip").lsp_expand(snippet)
+--             end,
+--             active = function(filter)
+--                 if filter and filter.direction then
+--                     return require("luasnip").jumpable(filter.direction)
+--                 end
+--                 return require("luasnip").in_snippet()
+--             end,
+--             jump = function(direction)
+--                 require("luasnip").jump(direction)
+--             end,
+--         },
+--         sources = {
+--             default = default_sources,
+--             cmdline = function()
+--                 local type = vim.fn.getcmdtype()
+--                 if type == "/" or type == "?" then
+--                     return { "buffer" }
+--                 end
+--                 if type == ":" then
+--                     return { "cmdline", "path" }
+--                 end
+--                 return {}
+--             end,
+--             providers = {
+--                 lsp = {
+--                     fallbacks = { "lazydev" },
+--                 },
+--                 lazydev = {
+--                     name = "LazyDev",
+--                     module = "lazydev.integrations.blink",
+--                 },
+--                 ripgrep = {
+--                     module = "blink-cmp-rg",
+--                     name = "Ripgrep",
+--                     opts = {
+--                         prefix_min_len = 3,
+--                         get_command = function(_, prefix)
+--                             return {
+--                                 "rg",
+--                                 "--no-config",
+--                                 "--json",
+--                                 "--word-regexp",
+--                                 "--ignore-case",
+--                                 "--",
+--                                 prefix .. "[\\w_-]+",
+--                                 vim.fs.root(0, ".git") or vim.fn.getcwd(),
+--                             }
+--                         end,
+--                         get_prefix = function(context)
+--                             return context.line:sub(1, context.cursor[2]):match("[%w_-]+$") or ""
+--                         end,
+--                     },
+--                 },
+--             },
+--         },
+--         appearance = {
+--             kind_icons = lsp_symbols,
+--         },
+--         completion = {
+--             accept = { auto_brackets = { enabled = true } },
+--             trigger = {
+--                 show_on_insert_on_trigger_character = false,
+--             },
+--             menu = {
+--                 draw = {
+--                     padding = 2,
+--                     gap = 1,
+--                     treesitter = { "lsp" },
+--                     columns = {
+--                         { "kind_icon" },
+--                         { "label", "label_description", gap = 1 },
+--                         { "kind" },
+--                     },
+--                     components = {
+--                         label = {
+--                             width = { fill = true, max = 60 },
+--                             text = function(ctx)
+--                                 local highlights_info = require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
+--                                 if highlights_info ~= nil then
+--                                     return highlights_info.text
+--                                 else
+--                                     return ctx.label
+--                                 end
+--                             end,
+--                             highlight = function(ctx)
+--                                 local highlights_info = require("colorful-menu").highlights(ctx.item, vim.bo.filetype)
+--                                 local highlights = {}
+--                                 if highlights_info ~= nil then
+--                                     for _, info in ipairs(highlights_info.highlights) do
+--                                         table.insert(highlights, {
+--                                             info.range[1],
+--                                             info.range[2],
+--                                             group = ctx.deprecated and "BlinkCmpLabelDeprecated" or info[1],
+--                                         })
+--                                     end
+--                                 end
+--                                 for _, idx in ipairs(ctx.label_matched_indices) do
+--                                     table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
+--                                 end
+--                                 return highlights
+--                             end,
+--                         },
+--                     },
+--                 },
+--                 cmdline_position = function()
+--                     if vim.g.ui_cmdline_pos ~= nil then
+--                         local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
+--                         return { pos[1] - 1, pos[2] }
+--                     end
+--                     local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+--                     return { vim.o.lines - height, 0 }
+--                 end,
+--             },
+--             documentation = {
+--                 auto_show = true,
+--                 auto_show_delay_ms = 10,
+--                 treesitter_highlighting = true,
+--             },
+--             ghost_text = {
+--                 enabled = true,
+--             },
+--         },
+--         keymap = {
+--             ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+--             ["<C-e>"] = { "hide", "fallback" },
+--             ["<CR>"] = { "accept", "fallback" },
+--             ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+--             ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+--             ["<Down>"] = { "select_next", "fallback" },
+--             ["<Up>"] = { "select_prev", "fallback" },
+--             ["<C-j>"] = { "select_next", "fallback" },
+--             ["<C-k>"] = { "select_prev", "fallback" },
+--             ["<C-h>"] = { "scroll_documentation_down", "fallback" },
+--             ["<C-l>"] = { "scroll_documentation_up", "fallback" },
+--             ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+--             ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+--             cmdline = {
+--                 ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+--                 ["<C-e>"] = { "hide", "fallback" },
+--                 ["<CR>"] = { "accept", "fallback" },
+--                 ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+--                 ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+--                 ["<Down>"] = { "select_next", "fallback" },
+--                 ["<Up>"] = { "select_prev", "fallback" },
+--                 ["<C-j>"] = { "select_next", "fallback" },
+--                 ["<C-k>"] = { "select_prev", "fallback" },
+--                 ["<C-h>"] = { "scroll_documentation_down", "fallback" },
+--                 ["<C-l>"] = { "scroll_documentation_up", "fallback" },
+--             },
+--         },
+--     })
+-- end
 
 config.lua_snippets = function()
     local config_path = vim.fn.stdpath("config")
