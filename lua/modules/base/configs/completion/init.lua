13779646df72b637855ce074455166e8549a3e9e
LVIM ORG <lvim@motion-agency.com>
2025-01-22 08:56:20 +0200

    Blink config

--- lua/modules/base/configs/completion/init.lua
+++ lua/modules/base/configs/completion/init.lua
@@ -12,6 +12,21 @@ config.blink_cmp = function()
     local default_sources = vim.list_extend({ "lsp", "path", "snippets", "buffer" }, ext)
     local trigger_text = ";"
     blink_cmp.setup({
+        enabled = function()
+            local disabled = false
+            local success, node = pcall(vim.treesitter.get_node)
+            disabled = disabled or (vim.bo.buftype == "prompt")
+            disabled = disabled or (vim.fn.reg_recording() ~= "")
+            disabled = disabled or (vim.fn.reg_executing() ~= "")
+            disabled = disabled
+                or (
+                    success
+                    and node ~= nil
+                    and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type())
+                )
+
+            return not disabled
+        end,
         snippets = {
             preset = "luasnip",
             expand = function(snippet)
